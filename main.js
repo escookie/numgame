d84e0238fda1a3674209468019428aa9a03cb376
choisojin <choisojin@naver.com>
2024-02-11 08:49:01 +0900

    second commit

--- main.js
+++ main.js
@@ -12,12 +12,14 @@ let comNum = 0
 let playButton = document.getElementById("play-button")
 let userInput = document.getElementById("user-input")
 let resultArea = document.getElementById("result-area")
+let resultText = document.querySelector(".result-text");
 let resetButton = document.getElementById("reset-button")
 let beforeNum = document.getElementById("before-num")
 let chanceArea = document.getElementById("chance-area")
 let chances = 10
 let gameOver = false
 let history = []; // 이전 숫자를 저장할 배열
+let resultAreaImg = document.querySelector(".main-img");
 
 playButton.addEventListener("click", play);
 document.addEventListener("keydown", function(event) {
@@ -42,12 +44,12 @@ function play(){
     let userValue = userInput.value;
     //유효성검사
     if(userValue<1 || userValue>100){
-        resultArea.textContent = "1과 100 사이 숫자를 써주세요!"
+        resultText.textContent = "1과 100 사이 숫자를 써주세요!"
         return;
     }
 
     if (history.includes(userValue)){
-        resultArea.textContent = "이미 입력한 숫자입니다!"
+        resultText.textContent = "이미 입력한 숫자입니다!"
         return;
     }//유효성검사 끝
 
@@ -62,12 +64,17 @@ function play(){
 
 
 
-    if(userValue < comNum){
-        resultArea.textContent = "업!"
+    if(userValue < comNum){      
+        resultAreaImg.src = "images/up.jpg";  
+        resultText.textContent = "업!";
+        
     } else if (userValue > comNum){
-        resultArea.textContent = "다운!"
+        resultAreaImg.src = "images/down.jpg";
+        resultText.textContent = "다운!";
     } else {
-        resultArea.textContent = "정답입니다!"
+        resultAreaImg.src = "images/fireworks.gif";
+        resultText.textContent = "정답입니다!";
+
         gameOver = true;
     }
 
@@ -81,14 +88,16 @@ function play(){
 
 function reset(){
     userInput.value = "";
+    resultAreaImg.src = "images/smile.jpg";
     RandomNum();
-    resultArea.textContent = "한판 더!"
+    resultText.textContent = "한판 더!"
     chances = 10
     chanceArea.textContent = `남은기회 : ${chances} 번`
     history = []; // 이전 숫자 배열을 초기화
     beforeNum.textContent = "이전숫자 : "
     gameOver = false;
     playButton.disabled = false;
+    
 }
 
 RandomNum();
